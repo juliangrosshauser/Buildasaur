965461a497513fb30f18c72f1d19df138877b6d4
Julian Grosshauser <julian@juliangrosshauser.com>
2015-06-22 17:53:02 +0200

    Add Xcode project

---
+++ BuildasaurTest/Source/Classes/Application/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  BuildasaurTest
+//
+//  Created by Julian Grosshauser on 22/06/15.
+//  Copyright (c) 2015 Julian Grosshauser. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
+
---
+++ BuildasaurTestTests/Source/Classes/BuildasaurTestTests.swift
@@ -0,0 +1,21 @@
+//
+//  BuildasaurTestTests.swift
+//  BuildasaurTestTests
+//
+//  Created by Julian Grosshauser on 22/06/15.
+//  Copyright (c) 2015 Julian Grosshauser. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class BuildasaurTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+}
