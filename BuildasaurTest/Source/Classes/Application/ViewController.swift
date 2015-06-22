9179ceaadbe87a27317a24653e9146a2c06a0a13
Julian Grosshauser <julian@juliangrosshauser.com>
2015-06-22 18:15:47 +0200

    Add ViewController

---
+++ BuildasaurTest/Source/Classes/Application/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  BuildasaurTest
+//
+//  Created by Julian Grosshauser on 22/06/15.
+//  Copyright (c) 2015 Julian Grosshauser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+}
